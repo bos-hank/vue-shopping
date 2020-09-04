d9e79d9f4fac25760f1a98223ef4b84e9e5c7e09
Hank Ye <hankye@Hank-MacBook-Air-v10.local>
2020-09-04 14:10:00 +0800

    90. 驗證登入及 Vue Router 的配置

--- src/main.js
+++ src/main.js
@@ -19,3 +19,21 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requiresAuth) {
+    const api = `${process.env.API_PATH}/api/user/check`;
+    axios.post(api).then((response) => {
+      console.log(response.data);
+      if(response.data.success) {
+        next();
+      } else {
+        next({ path: '/login' })
+      }
+    }); 
+  } else {
+    next();
+  }
+  // if (to.name !== 'Login' && !isAuthenticated) next({ name: 'Login' })
+  // else next()
+});
